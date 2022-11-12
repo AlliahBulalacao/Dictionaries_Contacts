49c316d9ea457b56aedb92c2b83d15a0b655b4b6
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-11-12 21:09:36 +0800

    Adding option 1

--- Dictionaries.py
+++ Dictionaries.py
@@ -2,7 +2,18 @@
 print("Menu:"
       "1 -> Add an item"
       "2 -> Search "
-      "3-> Exit (Yes or No)"
+      "3-> Exit (Yes or No")
 
 # Allow user to select item in the menu (check if valid)
 select = input("Select your chosen menu (1-3): ")
+
+# Perform the selected option
+
+if select == 1:
+      name = input("Full name: ")
+      age = input("Age: ")
+      org = input("Organization: ")
+      contact = input("Contact Number:")
+      address = input("Address: ")
+      personal_info = {contact : address, age : org}
+      information = {name : personal_info}
