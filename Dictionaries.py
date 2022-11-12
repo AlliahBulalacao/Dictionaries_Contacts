e24417ad261f560f1642b87e97a35a7f4bbf9306
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-11-12 21:53:53 +0800

    Editing Information

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,19 +1,35 @@
+# Dictionary
+
+name = input("Full name: ")
+contact = int(input("Contact Information: "))
+age = int(input("Age: "))
+address = input("Address: ")
+org = input("Organization: ")
+
+info = {"Contact Information": contact,
+        "Age" : age,
+        "Address" : address,
+        "Organization" : org
+}
+information = {
+      "Full name" : name,
+      "Personal Info" : info
+}
+print(info)
+print(information)
+
 # Display a menu of options
-print("Menu:"
-      "1 -> Add an item"
-      "2 -> Search "
-      "3-> Exit (Yes or No")
+print("Menu: \n"
+      "1 -> Add an item\n"
+      "2 -> Search \n"
+      "3-> Exit (Yes or No)")
 
 # Allow user to select item in the menu (check if valid)
-select = input("Select your chosen menu (1-3): ")
-
 # Perform the selected option
-
-if select == 1:
-      name = input("Full name: ")
-      age = input("Age: ")
-      org = input("Organization: ")
-      contact = input("Contact Number:")
-      address = input("Address: ")
-      personal_info = {contact : address, age : org}
-      information = {name : personal_info}
+while True:
+      select = input("Select your chosen menu (1-3): ")
+      if select == 1:
+            add_name = input("Full Name: ")
+            add_age = input("Age: ")
+            add_address = input("Address: ")
+            print(information)
