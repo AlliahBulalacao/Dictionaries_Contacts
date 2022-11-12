34a740b15fff49bd38b44ba5c751b1526a0e269b
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-11-12 21:02:10 +0800

    Select options

--- Dictionaries.py
+++ Dictionaries.py
@@ -3,3 +3,6 @@ print("Menu:"
       "1 -> Add an item"
       "2 -> Search "
       "3-> Exit (Yes or No)"
+
+# Allow user to select item in the menu (check if valid)
+select = input("Select your chosen menu (1-3): ")
